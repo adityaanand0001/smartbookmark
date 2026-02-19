c8435698d792fddb10a3f7f71516483e26d5baed
Aditya Anand <98987504+adityaanand0001@users.noreply.github.com>
2026-02-19 11:35:10 +0530

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://bauhstofdbyyjodclkcv.supabase.co'; 
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhdWhzdG9mZGJ5eWpvZGNsa2N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEzMzY4NDAsImV4cCI6MjA4NjkxMjg0MH0.K1NtRDIcYPKFD26-aEKzZqiCgOIZexHBFzRjw_VNrZQ';
-
+export const supabaseUrl = process.env.SUPABASE_URL; 
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY; 
 export const isUsingDemoKeys = false;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
